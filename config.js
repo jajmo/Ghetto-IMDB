65e34f7e903275b13d8cbb7f4c2043e74e3a7214
jajmo <jajmo@stevens.edu>
2016-04-26 14:05:05 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+var exports = module.exports = {};
+
+exports.mongoConfig = {
+    serverUrl: "mongodb://localhost:27017/",
+    database: "ghetto_imdb"        
+};
+
+exports.serverConfig = {
+    metadataSource: "",
+    markOption: ""
+};
---
+++ server.js
@@ -0,0 +1,74 @@
+// We first require our express package
+var express = require('express');
+var bodyParser = require('body-parser');
+var movieData = require('./data.js');
+
+// We create our express isntance:
+var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('static'));
+
+// If you'll notice, there's not a single database call in the server file!
+
+app.get("/", function (request, response) {
+    movieData.getAllMovies().then(function (movies) {
+        response.render("pages/index", { movies: movies });
+    });
+});
+
+// Get the best movies
+app.get("/api/movies/best", function(request, response) {
+    movieData.getPopularMovies().then(function(popularMovies){
+        response.json(popularMovies);
+    });
+});
+
+// Get a single movie
+app.get("/api/movies/:id", function(request, response) {
+    movieData.getMovie(request.params.id).then(function(movie) {
+        response.json(movie);
+    }, function(errorMessage) {
+        response.status(500).json({ error: errorMessage });
+    });
+});
+
+// Get all the movies
+app.get("/api/movies", function(request, response) {
+    movieData.getAllMovies().then(function(movieList) {
+        response.json(movieList);
+    });
+});
+
+// Create a movie
+app.post("/api/movies", function(request, response) {
+    movieData.createMovie(request.body.title, request.body.rating).then(function(movie) {
+        response.json(movie);
+    }, function(errorMessage) {
+        response.status(500).json({ error: errorMessage });
+    });
+});
+
+// Update a movie 
+app.put("/api/movies/:id", function(request, response) {
+    movieData.updateMovie(request.params.id, request.body.title, request.body.rating).then(function(movie) {
+        response.json(movie);
+    }, function(errorMessage) {
+        response.json({ error: errorMessage });
+    });
+});
+
+app.delete("/api/movies/:id", function(request, response) {
+    movieData.deleteMovie(request.params.id).then(function(status) {
+        response.json({success: status});
+    }, function(errorMessage) {
+        response.json({ error: errorMessage });
+    });
+});
+
+// We can now navigate to localhost:3000
+app.listen(3000, function() {
+    console.log('Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it');
+});

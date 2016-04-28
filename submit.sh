cac5960813845c04f3e6cb44af60d74731db991e
Bradford Smith <bsmith8@stevens.edu>
2016-04-28 18:01:11 -0400

    Added a Makefile for LaTeX
    Also updated the submit script to use that.

--- submit.sh
+++ submit.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 ################################################################################
 # CS 546 Final Project submit.sh
-# 04/26/2016
-# "I pledge my honor that I have abided by the Stevens Honor System."
+# 04/28/2016
+# "We pledge our honor that we have abided by the Stevens Honor System."
 ################################################################################
 
 ASSIGNMENT='cs546-final-project'
+
+# make sure LaTeX is compiled to PDF before submitting
+make -C documentation all
+make -C documentation clean
 
 # make tar.gz archive for easy submission
 tar -cvzf $ASSIGNMENT.tar.gz ./* --exclude='./node_modules' --exclude='*.zip'

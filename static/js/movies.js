eb590436c7f6731e0df68a089bb4441b1eb45365
patgrasso <pgrasso@stevens.edu>
2016-04-28 20:50:53 -0400

    Separate EJS components into different files
    Displace the nav bar, each movie panel, and each genre section.
    Additionally, remove the css from the EJS files and give them a
    suitable home in static/css.

--- static/js/movies.js
+++ static/js/movies.js
@@ -2,24 +2,37 @@
 
     // TODO: We're going to need data-* attributes to prevent expanding/collapsing/etc all of
     // the buttons at once
-    $("div").on("click", "#save-button", function (e) {
-        var id = $(this).attr("data-id");
-        $("#save-button-div-" + id).collapse("hide");
-        $("#watched-buttons-" + id).collapse("show");
+    $('div').on('click', '#save-button', function (e) {
+        var id = $(this).attr('data-id');
+        $('#save-button-div-' + id).collapse('hide');
+        $('#watched-buttons-' + id).collapse('show');
         return;
     });
 
-    $("div").on("click", ".watched-btn", function (e) {
-        var id = $(this).attr("data-id");
+    $('div').on('click', '.watched-btn', function (e) {
+        var id = $(this).attr('data-id');
 
-        $("#watched-buttons-" + id).collapse("hide");
-        $("#save-button[data-id='" + id + "']").attr("disabled", "disabled");
-        $("#save-button-div-" + id).collapse("show");
+        $('#watched-buttons-' + id).collapse('hide');
+        $('#save-button[data-id="' + id + '"]').attr('disabled', 'disabled');
+        $('#save-button-div-' + id).collapse('show');
     });
 
-    $("h2").on("click", ".btn-collapse", function (e) {
-        var text = $(this).html() === "Collapse" ? "Expand" : "Collapse";
+    $('h2').on('click', '.btn-collapse', function (e) {
+        var text = $(this).html() === 'Collapse' ? 'Expand' : 'Collapse';
         $(this).html(text);
+    });
+
+    // For every <li><a href=[path]></a></li>, if the path is equal to the current
+    // browser location, give the anchor the 'active' class so it is properly
+    // highlighted by bootstrap
+    $('.navbar .navbar-nav li').each(function (_, li) {
+        if (li.children instanceof HTMLCollection && li.children.length > 0) {
+            $(li.children).each(function (_, child) {
+                if (child.href === document.location.href) {
+                    li.classList.add('active');
+                }
+            });
+        }
     });
 
 })(window.jQuery);

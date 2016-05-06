b4d41e6c4ae3efe88dddeaf5b7d3d743ccac720e
patgrasso <pgrasso@stevens.edu>
2016-05-06 11:35:14 -0400

    Add matchers for individual form elements
    Allow individual inputs to be matched by regex. By default, inputs of a
    null or empty-string value are considered invalid, but if a matcher is
    specified for the input in the `matchers` parameter, it will be used as
    a regular expression to test the corresponding input on whichever form
    is being validated.
    
    Remove support for adding validation to multiple forms in one call
    (this is a rare case and disallows any other parameters from being
    passed into the validation function).
    
    Change the `Ghetto` global container to be a `var` instead of a `const`
    (consts are block-scoped and throw errors upon re-definition
    :frowning:).

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,42 +1,55 @@
 
-const Ghetto = {};
+var Ghetto;
+
+if (!Ghetto) { Ghetto = {}; }
 
 (function ($) {
 
     /**
-     * Sets up event listeners on forms specified as arguments to validate form
-     * input upon submission. If an input value is null or empty, its parent
-     * .form-group element will have the 'has-error' class assigned to it.
+     * Sets up an event listener on the form specified to validate form input upon
+     * submission. If an input value is null or empty, or does not match its
+     * matcher as specified in `matchers`, its parent `.form-group` element will
+     * have the 'has-error' class assigned to it.
      *
-     * @function validation
-     * @param {String...} arguments Each argument will be used to select a form
+     * @function addValidation
+     * @param {String} selector The selector used to find the form element
+     * @param {Object} matchers Mapping of form input names to regular expressions
+     *    that will be used to validate each input
+     * @param {String|RegExp} matchers[name] Regular expression used to validate
+     *    the form input value whose attribute name=`name`
      */
-    Ghetto.validation = function () {
-        $(Array.prototype.join.call(arguments, ',')).submit(function (e) {
+    Ghetto.addValidation = function (selector, matchers) {
+        var testers = {};
+
+        if (matchers != null && matchers instanceof Object) {
+            Object.keys(matchers).forEach(function (name) {
+                testers[name] = new RegExp(matchers[name]);
+            });
+        }
+
+        $(selector).submit(function (e) {
             var canSubmit = true;
             var formObj   = {};
 
-            e.preventDefault();
-
             $(this).find('input, textarea').each(function (i, elem) {
-                if (elem.value == null || elem.value === '') {
+                if (
+                    elem.value == null  ||
+                    elem.value === ''   ||
+                    (testers[elem.name] && !testers[elem.name].test(elem.value))
+                ) {
                     $(elem).parent().addClass('has-error');
                     canSubmit = false;
                 } else {
+                    $(elem).parent().removeClass('has-error');
                     formObj[elem.name] = elem.value;
                 }
             });
 
-            if (canSubmit) {
-                $.post('/post', formObj, function () {
-                    location.reload();
-                });
+            if (!canSubmit) {
+                e.preventDefault();
             }
         });
     };
 
-    $(document).ready(function () {
-        Ghetto.validation('form');
-    });
 })(window.jQuery);
 
